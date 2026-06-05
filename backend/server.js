54d977cf62baa019d74ebcdaa8fb0c0b4a7e6c7f
Dhanushri-D <dhanushri.d2024cse@sece.ac.in>
2026-06-05 12:53:34 +0530

    commit on server.js url update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://trip-nova-frontend.vercel.app'], credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routers/authRouter'));
